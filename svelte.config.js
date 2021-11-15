72d89f209ab2667028d2ca99ba0cd53a59a826cd
Nelson William <nelsonwilliam@live.com>
2021-11-15 18:37:12 -0300

    Second commit to fix deploy

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: adapter({
 			// default options are shown
